8d5cc376650462c2bbc2e3733dde9213ea119fb6
VladoSimic23 <57799129+VladoSimic23@users.noreply.github.com>
2023-09-03 14:15:33 +0200

    Update sanity.ts

--- src/sanity.ts
+++ src/sanity.ts
@@ -1,7 +1,7 @@
 import {createClient} from "@sanity/client";
 const apiUrl = import.meta.env.VITE_APP_API_SANITY;
 
-"n6wo2hxf"
+
 export default createClient({
   projectId: "n6wo2hxf", // find this at manage.sanity.io or in your sanity.json
   dataset: "production", // this is from those question during 'sanity init'
