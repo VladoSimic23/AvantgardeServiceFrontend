691c2266778e16bf0cc1706e7e9c4f5cba1413f9
VladoSimic23 <vladosimic525@gmail.com>
2023-09-11 17:32:32 +0200

    css update

--- src/sanity.ts
+++ src/sanity.ts
@@ -3,7 +3,7 @@ const apiUrl = import.meta.env.VITE_APP_API_SANITY;
 
 
 export default createClient({
-  projectId: 'n6wo2hxf', // find this at manage.sanity.io or in your sanity.json
+  projectId: apiUrl, // find this at manage.sanity.io or in your sanity.json
   dataset: "production", // this is from those question during 'sanity init'
   apiVersion: "2023-09-03",
   useCdn: true
