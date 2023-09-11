b435e7c33b9b61b67e781d70bf7f7afcd67916de
VladoSimic23 <vladosimic525@gmail.com>
2023-09-11 17:17:52 +0200

    Added lazy loding

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,7 @@
-// import HeroSection from "./HeroSection";
-import ExperienceSection from "./ExperienceSection";
-import GetToKnowUsSection from "./GetToKnowUsSection";
-import TestimonialsSection from "./TestimonialsSection";
 import {lazy,Suspense} from "react"
-
 const HeroSection = lazy(() => import("./HeroSection"));
+const ExperienceSection= lazy(() => import("./ExperienceSection"));
+const TestimonialsSection = lazy(() => import("./TestimonialsSection"));
 
 const Home = () => {
   return (
@@ -14,9 +11,16 @@ const Home = () => {
         </div>}>
           <HeroSection/>
         </Suspense>}
-        <ExperienceSection/>
-        <GetToKnowUsSection/>
-        <TestimonialsSection/>
+        { ExperienceSection && <Suspense fallback={<div>
+          <h1>Loading...</h1>
+        </div>}>
+          <ExperienceSection/>
+        </Suspense>}
+        { TestimonialsSection && <Suspense fallback={<div>
+          <h1>Loading...</h1>
+        </div>}>
+          <TestimonialsSection/>
+        </Suspense>}
     </div>
   )
 }
