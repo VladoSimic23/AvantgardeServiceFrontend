416637a8570d29c361c82abcd6331de0177f06c9
VladoSimic23 <vladosimic525@gmail.com>
2023-09-11 18:15:30 +0200

    added lazy loading

--- src/components/videos/Videos.tsx
+++ src/components/videos/Videos.tsx
@@ -7,12 +7,11 @@ const Videos = ({videos}: any) => {
         {videos && videos.map((vid:any,index: number) => {
             return <div key={index}>
                         <iframe
-                         width="100%"
+                        width="100%"
                         height="600"
                         src={vid}
                         title="YouTube video player"
                         frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                         allowFullScreen
                         ></iframe>
                     </div>
