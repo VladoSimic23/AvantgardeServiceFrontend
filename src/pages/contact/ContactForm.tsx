02bd1fd1d567eabd5eb4c5c224ddf1618ae351db
VladoSimic23 <vladosimic525@gmail.com>
2023-10-01 10:07:24 +0200

    Contact form updates

--- src/pages/contact/ContactForm.tsx
+++ src/pages/contact/ContactForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import emailjs from "emailjs-com"
 import styles from "./css/contact.module.css"
 import globalStyles from "../../globalCss/globalStyle.module.css"
@@ -7,6 +7,11 @@ function ContactForm() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const [subject, setSubject] = useState('');
+  const [messageStatus,setMessageStatus] = useState("");
+
+
+  
 
   const handleSubmit = async (e:any) => {
     e.preventDefault();
@@ -15,15 +20,28 @@ function ContactForm() {
       const response = await emailjs.send(
         'service_bnyldgl',
         'template_hg9nfq9',
-        { from_name: name, from_email: email, message },
+        { from_name: name,from_subject: subject ,from_email: email, message },
         "ZHX9qWZnI66p5L68e"
       );
 
-      console.log('Email sent successfully:', response);
+      setMessageStatus('Email sent successfully');
+      setName(""),
+      setEmail(""),
+      setMessage(""),
+      setSubject("")
     } catch (error) {
-      console.error('Error sending email:', error);
+      setMessageStatus(`Error sending email: ${error}`);
     }
   };
+
+  useEffect(() => {
+    if(messageStatus) {
+      setTimeout(() => {
+        setMessageStatus("")
+      },3000)
+    }
+  },[messageStatus])
+
 
   return (
     <>
@@ -31,9 +49,16 @@ function ContactForm() {
       <input
         type="text"
         placeholder="Name"
-        value={name}
         name='theName'
+        value={name}
         onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Subject"
+        name='theSubject'
+        value={subject}
+        onChange={(e) => setSubject(e.target.value)}
       />
       <input
         type="email"
@@ -48,6 +73,7 @@ function ContactForm() {
         value={message}
         onChange={(e) => setMessage(e.target.value)}
       />
+       {messageStatus && <h3>{messageStatus}</h3>}
       <div className={styles.submitBtn}>
         <button className={globalStyles.btnGreen} type="submit">Send Message</button>
       </div>
